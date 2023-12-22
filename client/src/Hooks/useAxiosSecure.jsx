77640b2812d617f1965b1c752556cf0b1ded8806
Ahammad Abdulla <ahammadabdullahnew@gmail.com>
2023-12-22 09:40:37 +0600

    fix: refector

--- client/src/Hooks/useAxiosSecure.jsx
+++ client/src/Hooks/useAxiosSecure.jsx
@@ -6,22 +6,21 @@ const axiosSecure = axios.create({
 });
 
 // intercept response and check for unauthorized responses.
-axiosSecure.interceptors.response.use(
-  (response) => response,
-  async (error) => {
-    console.log("Error tracked in the interceptor", error.response);
-    if (
-      error.response &&
-      (error.response.status === 401 || error.response.status === 403)
-    ) {
-      await axiosSecure.post("/logout");
-      window.location.replace("/login");
-      handleLogout();
-    }
+// axiosSecure.interceptors.response.use(
+//   (response) => response,
+//   async (error) => {
+//     console.log("Error tracked in the interceptor", error.response);
+//     if (
+//       error.response &&
+//       (error.response.status === 401 || error.response.status === 403)
+//     ) {
+//       await axiosSecure.post("/logout");
+//       window.location.replace("/login");
+//     }
 
-    return Promise.reject(error);
-  }
-);
+//     return Promise.reject(error);
+//   }
+// );
 
 const useAxiosSecure = () => {
   return axiosSecure;
